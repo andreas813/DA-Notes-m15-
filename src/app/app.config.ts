d4195b1a6b79a5459084cb4048541d89cf3d6d23
Andreas W. <bahamut13+github0@gmail.com>
2025-04-04 22:37:25 +0200

    firebase setup, first functions in note-list.service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"da-notes-a2ab3","appId":"1:70162511196:web:81a3113a610f81a8123d99","storageBucket":"da-notes-a2ab3.firebasestorage.app","apiKey":"AIzaSyA9Ggj7FMV_tQqi1qMVfm23YDrjoDhb6gE","authDomain":"da-notes-a2ab3.firebaseapp.com","messagingSenderId":"70162511196"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,71 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Firestore, collectionData, collection, doc, DocumentData, onSnapshot } from '@angular/fire/firestore';
+import { elementAt, Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+    // items$: Observable<DocumentData[]>;
+
+    items$;
+    items;
+
+    unsubList: any;
+    unsubSingle: any;
+
+    firestore = inject(Firestore);
+
+    constructor() {
+
+        this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+            list.forEach(element => {
+                console.log(element);
+            });
+        });
+
+        this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "r321f2qf2f2f2f"), (element) => { });
+
+        // this.unsubSingle();
+
+        this.items$ = collectionData(this.getNotesRef());
+        this.items = this.items$.subscribe((list) => {
+            list.forEach(element => {
+                console.log(this.setNoteObject(element['data'], element['id']));
+            });
+        });
+
+        // this.items$ = collectionData(this.getNotesRef());
+    }
+
+    setNoteObject(obj: any, id: string): Note {
+        return {
+            id: id || "",
+            type: obj.type || "note",
+            title: obj.title || "",
+            content: obj.content || "",
+            marked: obj.marked || false,
+        }
+    }
+
+    ngonDestroy() {
+        this.unsubList();
+        this.items.unsubscribe();
+
+    }
+
+    getNotesRef() {
+        return collection(this.firestore, 'notes');
+    }
+
+    getTrashRef() {
+        return collection(this.firestore, 'trash');
+    }
+
+    getSingleDocRef(collectionId: string, docId: string) {
+        return doc(collection(this.firestore, collectionId), docId);
+    }
 }
